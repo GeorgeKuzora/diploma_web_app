762c07f82cabc96111b51fd0a7db7fbc320bc7b4
GeorgiyKuzora <rafale87@gmail.com>
2023-07-10 18:54:13 +0300

    Added mysql confit to settings and my.cnf template

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = "web_app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        },
     }
 }
 
