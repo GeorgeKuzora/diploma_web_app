f9d3979f545d53a679cae3c28e57bafe2f7e3b0b
GeorgiyKuzora <rafale87@gmail.com>
2023-08-28 20:49:47 +0300

    Added arguments to http_request_handler

--- tests/test_http_request_handler.py
+++ tests/test_http_request_handler.py
@@ -4,15 +4,35 @@ import pytest
 import web_scraping.scrapers.request_handlers.http_request_handler as hrh
 
 
+DEFAULT_REQUEST_VALUES = {"text": "python", "area": 2, "page": 0}
+
+
 @pytest.mark.asyncio
-async def test_send_get_request():
+async def test_send_get_request_without_user_agent():
     session = aiohttp.ClientSession()
     handler = hrh.HttpRequestHandler()
-    ua = UserAgent()
     response = await handler.send_get_request(
-        url="http://httpbin.org/get", session=session, params=ua.random
+        url="https://hh.ru/search/vacancy",
+        session=session,
+        params=DEFAULT_REQUEST_VALUES,
     )
-    assert 200 == response[0]
+    assert response
+    assert "python" in response
+
+
+@pytest.mark.asyncio
+async def test_send_get_request_with_user_agent():
+    session = aiohttp.ClientSession()
+    ua = UserAgent()
+    handler = hrh.HttpRequestHandler()
+    response = await handler.send_get_request(
+        url="https://hh.ru/search/vacancy",
+        session=session,
+        params=DEFAULT_REQUEST_VALUES,
+        headers={"User-Agent": ua.random},
+    )
+    assert response
+    assert "python" in response
 
 
 @pytest.mark.asyncio
--- web_scraping/scrapers/request_handlers/http_request_handler.py
+++ web_scraping/scrapers/request_handlers/http_request_handler.py
@@ -1,15 +1,17 @@
 import aiohttp
+import asyncio
 
 
 class HttpRequestHandler:
-    def __init__(self, session=aiohttp.ClientSession()) -> None:
-        self.session = session
-
     async def send_get_request(
-        self, url: str, session: aiohttp.ClientSession, **kwargs
-    ) -> tuple[int, str]:
-        resp = await session.get(url=url, **kwargs)
+        self,
+        url: str,
+        session: aiohttp.ClientSession,
+        params: dict | None = None,
+        headers: dict | None = None,
+        *args, **kwargs
+    ) -> str:
+        resp = await session.get(url=url, params=params, headers=headers, *args, **kwargs)
         resp.raise_for_status()
-        status = resp.status
         html = await resp.text()
-        return status, html
+        return html
