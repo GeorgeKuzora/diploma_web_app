12de859db51b430ba426c9a26a81b6c1d82a659d
GeorgiyKuzora <rafale87@gmail.com>
2023-09-04 21:35:29 +0300

    Added checks for null results in parser module and tests

--- tests/test_hh_parser.py
+++ tests/test_hh_parser.py
@@ -18,23 +18,43 @@ def job_page_html():
     return html
 
 
-def test_get_number_of_job_list_pages(paginated_list_of_job_cards_html):
-    parser = hhp.hh_parser()
+@pytest.fixture
+def empty_page_html():
+    with open("tests/mock_pages/empty.html", "r") as file:
+        html = file.read()
+    assert html is not None
+    return html
+
+
+def test_get_number_of_job_list_pages(
+    paginated_list_of_job_cards_html, empty_page_html
+):
+    parser = hhp.HHParser()
     pages_number = parser.get_number_of_job_list_pages(paginated_list_of_job_cards_html)
     assert pages_number == 28
+    pages_number = parser.get_number_of_job_list_pages(empty_page_html)
+    assert pages_number == 0
 
 
-def test_get_hrefs_for_job_list_pages(paginated_list_of_job_cards_html):
+def test_get_hrefs_for_job_list_pages(
+    paginated_list_of_job_cards_html, empty_page_html
+):
     test_number_of_job_list_hrefs = 51
-    parser = hhp.hh_parser()
+    parser = hhp.HHParser()
     job_card_hrefs = parser.get_href_of_job_cards(paginated_list_of_job_cards_html)
     assert len(job_card_hrefs) == test_number_of_job_list_hrefs
+    for h in job_card_hrefs:
+        assert isinstance(h, str)
+    print(job_card_hrefs)
+    job_card_hrefs = parser.get_href_of_job_cards(empty_page_html)
+    assert job_card_hrefs == []
 
 
-def test_get_job_card_data(job_page_html):
+def test_get_job_card_data(job_page_html, empty_page_html):
     test_number_of_skills = 5
-    parser = hhp.hh_parser()
+    parser = hhp.HHParser()
     skills = parser.get_job_card_data(job_page_html)
     assert len(skills["skills"]) == test_number_of_skills
     assert all(skills["skills"])
-
+    skills = parser.get_job_card_data(empty_page_html)
+    assert skills["skills"] == []
--- web_scraping/scrapers/parsers/hh_parser.py
+++ web_scraping/scrapers/parsers/hh_parser.py
@@ -1,46 +1,73 @@
+from typing import Optional
 from bs4 import BeautifulSoup as bs
 
 
-class hh_parser:
+class HHParser:
+    DEFAULT_PAGE_NUMBER = 0
+
     def __init__(self) -> None:
         pass
 
     def get_number_of_job_list_pages(self, html: str) -> int:
         soup: bs = bs(html, "html.parser")
-        page_count = (
-            int(
-                soup.find("div", attrs={"class": "pager"})
-                .find_all("span", recursive=False)[-1]
-                .find("a", attrs={"class": "bloko-button"})
-                .find("span")
-                .text
+        pager_tag = soup.find("div", attrs={"class": "pager"})
+        if pager_tag:
+            page_count = (
+                int(
+                    pager_tag.find_all("span", recursive=False)[-1]
+                    .find("a", attrs={"class": "bloko-button"})
+                    .span.text
+                )
+                - 1
             )
-            - 1
-        )
-        return page_count
+            return int(page_count)
+        return self.DEFAULT_PAGE_NUMBER
 
-    def get_href_of_job_cards(self, html: str) -> list[str]:
+    def get_href_of_job_cards(self, html: str) -> list[Optional[str]]:
         soup: bs = bs(html, "html.parser")
-        return list(
-            map(
-                lambda a: a.attrs["href"].split("?")[0],
-                soup.find(
-                    "div",
-                    attrs={
-                        "data-qa": "vacancy-serp__results",
-                        "id": "a11y-main-content",
-                    },
-                ).find_all(
-                    "a",
-                    attrs={"class": "serp-item__title", "data-qa": "serp-item__title"},
-                ),
-            )
+        stripped_hrefs = []
+        href_for_job_cards = []
+        main_content = soup.find(
+            "div",
+            attrs={
+                "data-qa": "vacancy-serp__results",
+                "id": "a11y-main-content",
+            },
         )
+        if main_content:
+            for a in main_content.find_all(
+                "a",
+                attrs={
+                    "class": "serp-item__title",
+                    "data-qa": "serp-item__title",
+                },
+            ):
+                if a:
+                    stripped_hrefs.append(str(a.get("href").split("?")[0]))
+        return stripped_hrefs
 
     def get_job_card_data(self, html: str) -> dict:
         skills = []
         soup: bs = bs(html, "html.parser")
         skills_div = soup.find("div", attrs={"class": "bloko-tag-list"})
-        for skill in skills_div.children:
-            skills.append(str(skill.span.string))
+        if skills_div:
+            for skill in skills_div.children:
+                skills.append(str(skill.find("span").text))
         return {"skills": skills}
+
+
+# Back-end developer (Django, Python)
+#
+# от 120 000 до 120 000 ₽ на руки
+#
+# Требуемый опыт работы: 3–6 лет
+#
+# Полная занятость, полный день
+#
+# Описание
+#
+# Ключевые навыки <- 1
+#
+# ООО Автоматизация ПРО
+#
+# Санкт-Петербург, Петроградская
