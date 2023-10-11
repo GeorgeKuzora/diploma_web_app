f05aac1d6502f9db2609ed4b628cb177cddf2230
GeorgiyKuzora <rafale87@gmail.com>
2023-10-11 16:51:54 +0300

    Added company model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    title = models.CharField("company title", max_length=100)
+    description = models.TextField("company description")
+    email = models.EmailField("company email")
+    phone = models.CharField("company phone", max_length=12)
+    address = models.ForeignKey(
+        "Address", on_delete=models.SET_NULL, verbose_name="address"
+    )
