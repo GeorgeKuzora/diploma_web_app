7d141af0ffe541f82ab3fd280c5f7b508a1ae66c
GeorgiyKuzora <rafale87@gmail.com>
2023-10-14 11:36:57 +0300

    Created User model for users app
    Created User model for usres app.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+from datetime import date
 from django.db import models
+from skills.models import Skill
 
-# Create your models here.
+
+class User:
+    name = models.CharField("user name", max_length=20)
+    lastname = models.CharField("user lastname", max_length=30)
+    parentname = models.CharField("user parent name", max_length=20)
+    birthdate = models.DateField()
+    email = models.EmailField("user email")
+    password = models.CharField("user password")
+    registration_date = models.DateField(default=date.today())
+    skills = models.ManyToManyField(Skill)
+    phone = models.CharField("company phone", max_length=12)
+    address = models.ForeignKey(
+        "Address", on_delete=models.SET_NULL, verbose_name="address"
+    )
+
+    class META:
+        ordering = ["lastname", "name", "parentname"]
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return f"{self.lastname} {self.name} {self.parentname}"
