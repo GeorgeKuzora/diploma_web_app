2955dd94c2c2c0d6d91c16ff26e58bbe332c1445
GeorgiyKuzora <rafale87@gmail.com>
2023-10-19 16:45:07 +0300

    feature: Changed index view to show jobs based on the user's skills

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,30 @@
+from django.db.models import QuerySet
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
+from skills.models import Skill
+
+from users.models import Profile
 from .models import Job
 from .forms import JobForm
 from django.views import View
 
 
-def index(request):
-    job_list = Job.objects.order_by("job_name")
+def index(request) -> HttpResponse:
+    user: User = request.user
+    job_list: QuerySet[Job] | None = None
+    if user.is_anonymous or not user.is_active:
+        job_list = Job.objects.all()
+    if user.is_authenticated and user.is_active:
+        profile: Profile = Profile.objects.get(user=user)
+        skills = Skill.objects.filter(profile=profile)
+        if len(skills) > 0:
+            job_list = Job.objects.filter(skills__in=skills).distinct()
+        else:
+            job_list = Job.objects.all()
+    if not job_list:
+        job_list = Job.objects.all()
+
     context = {"job_list": job_list, }
     return render(request, "jobs/index.html", context)
 
