e4997674bd44e0034023a419f4f2513f10330120
GeorgiyKuzora <rafale87@gmail.com>
2023-10-19 09:17:30 +0300

    feature: Added base form for user Settings

--- users/forms.py
+++ users/forms.py
@@ -15,3 +15,15 @@ class UserAuthForm(forms.Form):
 
 class UserRegisterForm(BaseUserCreationForm):
     email = forms.EmailField(help_text="Enter your email")
+
+
+class UserSettingsForm(forms.Form):
+    username = forms.CharField(help_text="User's username")
+    firstname = forms.CharField(help_text="User's firstname")
+    lastname = forms.CharField(help_text="User's lastname")
+    parent_name = forms.CharField(help_text="User's parentname")
+    email = forms.EmailField(help_text="User's email")
+    birthdate = forms.DateField(help_text="User's birthdate")
+    skills = forms.TextInput()
+    phone = forms.CharField(help_text="User's phone")
+    address = forms.CharField(help_text="User's address")
