8a702aafcde3e8f8ab6c88283ab5a36acf6567d5
GeorgiyKuzora <rafale87@gmail.com>
2023-10-30 16:26:37 +0300

    docs: Added docstrings to companies app models module

--- web_app/companies/models.py
+++ web_app/companies/models.py
@@ -1,7 +1,24 @@
+"""
+Contains companies app models.
+
+Content: Company model, Address model.
+"""
 from django.db import models
 
 
 class Company(models.Model):
+    """
+    Companies registered as jobs providers.
+
+    Company model is used in jobs app.
+
+    Attributes:
+        title: models.CharField Company title.
+        description: models.TextField Long description of the company.
+        email: models.EmailField Company's email.
+        phone: models.CharField Company's phone.
+        address: models.ForeignKey('Address') Company's address.
+    """
     title = models.CharField("company title", max_length=100)
     description = models.TextField("company description")
     email = models.EmailField("company email")
@@ -20,12 +37,28 @@ class Company(models.Model):
 
 
 class Address(models.Model):
+    """
+    Registered addresses.
+
+    Address model is used in jobs and users apps.
+
+    Attributes:
+        country: models.CharField(choices) Address country.
+        region: models.CharField Address region.
+        city: models.CharField Address city.
+        srteet: models.CharField Address srteet.
+        building: models.CharField Address building.
+        room: models.CharField Address room.
+    """
+
     class CountryChoices(models.TextChoices):
         RUSSIA = "RUS"
         KAZAKHSTAN = "KAZ"
         BELARUS = "BEL"
 
-    country = models.CharField("country", max_length=50, choices=CountryChoices.choices)
+    country = models.CharField("country",
+                               max_length=50,
+                               choices=CountryChoices.choices)
     region = models.CharField("region", max_length=50)
     city = models.CharField("city", max_length=50)
     street = models.CharField("street", max_length=50)
@@ -38,4 +71,4 @@ class Address(models.Model):
         verbose_name_plural = "addresses"
 
     def __str__(self):
-        return f"{self.country}, {self.region}, {self.city}, {self.street}, {self.building}, {self.room}"
+        return f"{self.country},{self.region}, {self.city}, {self.street}, {self.building}, {self.room}"
